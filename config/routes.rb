3108fcd62564083c0fbf36ebbd82564010ef9c4c
Cheryl Chua <cherylsychua@outlook.com>
2018-02-28 12:25:32 +0000

    fixed routes for showing group

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'dashboard', to: 'pages#dashboard'
-  get 'vouchers/:name', to: 'vouchers#show_group', as: 'vouchers_group'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :vouchers do
     resources :bookings, only: [:create]
   end
 
-  # resources :users, only: [] do
-  #   resources :vouchers
-  # end
+  get 'vouchers/group/:name', to: 'vouchers#show_group', as: 'vouchers_group'
 end
