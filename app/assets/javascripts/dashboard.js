4ed0a7f2c46f569b5ebfe1d78992e8ecf1c73c36
Cheryl Chua <cherylsychua@outlook.com>
2018-03-02 15:09:03 +0000

    js change

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,25 +1,25 @@
-var listings_tab = document.getElementById("listings-tab");
+const listings_tab = document.getElementById("listings-tab");
 listings_tab.addEventListener(("click"), (event) => {
   switch_active_class(listings_tab);
 });
 
-var bookings_tab = document.getElementById("bookings-tab");
+const bookings_tab = document.getElementById("bookings-tab");
 bookings_tab.addEventListener(("click"), (event) => {
   switch_active_class(bookings_tab);
 });
 
-var watching_tab = document.getElementById("watching-tab");
+const watching_tab = document.getElementById("watching-tab");
 watching_tab.addEventListener(("click"), (event) => {
   switch_active_class(watching_tab);
 });
 
-var following_tab = document.getElementById("following-tab");
+const following_tab = document.getElementById("following-tab");
 following_tab.addEventListener(("click"), (event) => {
   switch_active_class(following_tab);
 });
 
 const switch_active_class = (tab) => {
-  var current_active_class = document.querySelector('.active');
+  const current_active_class = document.querySelector('.active');
   current_active_class.classList.remove('active');
   tab.classList.add('active');
 };
