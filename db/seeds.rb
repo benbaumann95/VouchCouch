f4250b454bc374fc6aa700d451a8f79165f33d09
Amresh Selvaskandan <amresh911@gmail.com>
2018-03-02 13:48:51 +0000

    changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# # This file should contain all the record creation needed to seed the database with its default values.
+# # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# #
+# # Examples:
+# #
+# #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+# #   Character.create(name: 'Luke', movie: movies.first)
 
 
-User.create!(email: 'lewagon@uni.com', first_name: 'Test', last_name: 'Test', password: 'password')
-Voucher.create!(name: 'Amazon Gift Card', category: 'Books', price: 20, end_date: Date.tomorrow(), quantity_left: 5, user_id: 1)
+# User.create!(email: 'lewagon@uni.com', first_name: 'Test', last_name: 'Test', password: 'password')
+# Voucher.create!(name: 'Amazon Gift Card', category: 'Books', price: 20, end_date: Date.tomorrow(), quantity_left: 5, user_id: 1)
 
-Voucher.create!(name: 'VUE Voucher', category: 'Movies', price: 10, end_date: Date.tomorrow(), quantity_left: 2, user_id: 1)
+# Voucher.create!(name: 'VUE Voucher', category: 'Movies', price: 10, end_date: Date.tomorrow(), quantity_left: 2, user_id: 1)
